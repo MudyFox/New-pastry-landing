26e0c96ba54e0767becac4f65aa506b4be2f4c2f
Sergey <mrlyxnwot@gmail.com>
2019-09-15 13:37:54 +0300

    add upgrade pastry landing

--- js/script.js
+++ js/script.js
@@ -1,42 +1,52 @@
-$("#navToggle").click(function() {
-    $(this).toggleClass("active");
-    $(".overlay").toggleClass("open");
-    // this line ▼ prevents content scroll-behind
-    $("body").toggleClass("locked");
-});
-$('.overlay').click(function() {
-    $(this).removeClass('open');
-    $('.navBurger').removeClass('active');
-});
+$(function () {
+    $("#navToggle").on('click', function () {
+        $(this).toggleClass("active");
+        $(".overlay").toggleClass("open");
+        // this line ▼ prevents content scroll-behind
+        $("body").toggleClass("locked");
+    });
+    $('.overlay').on('click', function () {
+        $(this).removeClass('open');
+        $('.navBurger').removeClass('active');
+    });
 
 
-var swiper = new Swiper('.swiper-container', {
-    navigation: {
-        nextEl: '.swiper-button-next' ,
-        prevEl: '.swiper-button-prev',
-    },
-    slidesPerView: 3,
-    loop: true,
-    autoplay: true,
-    delay: 1000,
-    breakpoints:{
-        1200:{
-
+    let swiper = new Swiper('.swiper-container', {
+        navigation: {
+            nextEl: '.swiper-button-next' ,
+            prevEl: '.swiper-button-prev',
         },
-        1000:{
-            slidesPerView: 2,
-        },
-        780:{
-            slidesPerView: 1,
+        slidesPerView: 3,
+        loop: true,
+        autoplay: true,
+        delay: 1000,
+        breakpoints:{
+            1200:{
 
-        },
-        400:{
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
             },
-            slidesPerView: 1,
-        }
+            1000:{
+                slidesPerView: 2,
+            },
+            780:{
+                slidesPerView: 1,
 
-    },
-});
+            },
+            400:{
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+                slidesPerView: 1,
+            }
+
+        },
+    });
+    $(function(){
+        $('a[href^="#"]').on('click', function () {
+            let target = $(this).attr('href');
+            $('html, body').animate({scrollTop: $(target).offset().top}, 800);
+            return false;
+        });
+    });
+
+})
